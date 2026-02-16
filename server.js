0724a1775dc62fc7c354f254e061343c44c9967f
M0zy03 <chikurov-ilya@inbox.ru>
2026-02-16 13:49:22 +0400

    Format server.js with consistent syntax

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.get("/leaderboard", async (req, res) => {
   res.json(top);
 });
 
-const PORT = process.env.PORT || 3000;
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server started on port " + PORT));
+
 
