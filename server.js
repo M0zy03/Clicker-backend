b6f4eebced5310d1da21651ee05e79726ba69139
M0zy03 <chikurov-ilya@inbox.ru>
2026-02-16 13:19:14 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://admin:Ilia_155154@cluster0.r5axfz4.mongodb.net/clicker?appName=Cluster0");
+
+const UserSchema = new mongoose.Schema({
+  telegramId: String,
+  score: { type: Number, default: 0 },
+  level: { type: Number, default: 1 },
+  multiplier: { type: Number, default: 1 }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/click", async (req, res) => {
+  const { telegramId } = req.body;
+
+  let user = await User.findOne({ telegramId });
+
+  if (!user) {
+    user = new User({ telegramId });
+  }
+
+  user.score += user.multiplier;
+  await user.save();
+
+  res.json(user);
+});
+
+app.get("/leaderboard", async (req, res) => {
+  const top = await User.find().sort({ score: -1 }).limit(10);
+  res.json(top);
+});
+
+app.listen(3000, () => console.log("Server started"));
